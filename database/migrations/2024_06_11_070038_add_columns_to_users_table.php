e642bde21f7cfe11ce8e7ecefc5841020306209e
laravel-dev12 <laravel.developer12@gmail.com>
2024-06-11 12:55:45 +0530

    DB migration file created. DB seeder changes added.

---
+++ database/migrations/2024_06_11_070038_add_columns_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone', 20)->nullable();
+            $table->enum('notification_switch', [0,1])->default(0);            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['phone', 'notification_switch']);
+        });
+    }
+}
